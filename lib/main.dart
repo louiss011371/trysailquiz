c48f6b1dccbe3783807f48dcad1a4eebc138957d
louiss011371 <louiss011371@hotmail.com>
2020-03-05 19:32:30 +0800

    Delete unuse import

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'home.dart';
-import 'package:flutter/rendering.dart';
-import 'customBottomNavigationBarWidget.dart';
-import 'drawerList.dart';
-import 'model/news.dart';
-import 'list.dart';
-import 'dart:convert';
 
 void main() => runApp(mainpage());
 var APP_BAR = 'TrySail Quiz　開発中';
